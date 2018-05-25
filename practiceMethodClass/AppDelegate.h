6ce5f2a881579ae942fc31efded83336ca5479a9
tabularasa0722 <tabularasa0722@gmail.com>
2018-05-25 20:02:24 +0900

    practiceMethodClass 既存のプロジェクトをそのまま乗っけてみた

---
+++ practiceMethodClass/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  practiceMethodProperty0528
+//
+//  Created by apple on 2018/05/23.
+//  Copyright © 2018年 com.nainai0722. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+@property(strong, nonatomic) ViewController *viewController; 
+
+
+@end
+
---
+++ practiceMethodClass/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  practiceMethodProperty0528
+//
+//  Created by apple on 2018/05/23.
+//  Copyright © 2018年 com.nainai0722. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
